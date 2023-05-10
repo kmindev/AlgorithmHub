22eb7a2533de62734cc936590600b1daf66756a8
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-05-10 23:19:23 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,15 @@
+class Solution {
+	public int solution(int[] common) {
+		int answer = 0;
+
+		// 등차 수열
+		if (common[1] - common[0] == common[2] - common[1])
+			answer = common[common.length - 1] + common[1] - common[0];
+
+		else // 등비 수열
+			answer = common[common.length - 1] * common[1] / common[0];
+
+		return answer;
+
+	}
+}
