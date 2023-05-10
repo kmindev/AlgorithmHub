dadf7b80818ffcc650067cdb55e8b7a8267062b7
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-05-10 23:27:19 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+
+        for(String input : keyinput) {
+            if(input.equals("left") && answer[0] > -board[0]/2 ) {
+                answer[0]--;
+            } else if (input.equals("down") && answer[1] > -board[1]/2 ) {
+                answer[1]--;
+            } else if (input.equals("up") && answer[1] < board[1]/2 ) {
+                answer[1]++;
+            } else if(input.equals("right") && answer[0] < board[0]/2 ){
+                answer[0]++;
+            }
+        }
+        return answer;
+    }
+}
