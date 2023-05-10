c3b56ed0e9777c6dce4d6a592b9deed8a18e755e
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-05-11 00:02:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,17 +2,18 @@ class Solution {
     public int solution(int[] numbers) {
     	int answer = 0;
     	boolean[] isCheck = new boolean[10];
+        
     	for(int i=1; i<=9; i++) {
     		for(int num : numbers) {
     			if(num==i) {
-    				isCheck[num] = true;
+    				isCheck[num] = true; // 하나라도 존재하면 true
     			}
     		}
     	}
     	
-    	for(int i=0; i<isCheck.length; i++) {
+    	for(int i=1; i<isCheck.length; i++) {
     		if(isCheck[i]==false)
-    			answer+=i;
+    			answer+=i; // false 인 걸 모두 더한다 
     	}
 
         return answer;
