4f74c05cf9c780266f6457c03672033f10960777
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-05-18 02:49:06 +0900

    [D2] Title: 파리 퇴치, Time: 151 ms, Memory: 22,640 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,43 +1,39 @@
-import java.util.Arrays;
-
 import java.util.Scanner;
 
 // 파리채
 public class Solution {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
 
-		int T = sc.nextInt();
+        int T = sc.nextInt();
 
-		for (int tc = 0; tc < T; tc++) {
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			int max = 0;
-			int[][] arr = new int[N][N];
+        for (int tc = 1; tc <= T; tc++) {
+            int N = sc.nextInt(); // 배열 크기
+            int M = sc.nextInt(); // 파리채 크기
+            int[][] arr = new int[N][N];
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            int max = 0;
+            for (int i = 0; i <= N - M; i++) {
+                for (int j = 0; j <= N - M; j++) {
+                    int sum = 0;
+                    for (int x = 0; x < M; x++) {
+                        for (int y = 0; y < M; y++) {
+                            sum += arr[i + x][j + y];
+                        }
+                    }
+                    max = Math.max(max, sum);
+                }
+            }
+            System.out.println("#" + tc + " " + max);
+        }
 
-			for (int i = 0; i < arr.length; i++) {
-				for (int j = 0; j < arr[i].length; j++) {
-					arr[i][j] = sc.nextInt();
-				}
-			}
 
-			for (int i = 0; i < N - M + 1; i++) {
-				for (int j = 0; j < N - M + 1; j++) {
-					int sum = 0;
-					for (int x = 0; x < M; x++) {
-						for (int y = 0; y < M; y++) {
-							sum += arr[x + i][y + j];
-						}
-					}
-					max = Math.max(sum, max);
-				}
-			}
-
-			System.out.println("#" + (tc + 1) + " " + max);
-
-		}
-	}
+    }
 
 }
