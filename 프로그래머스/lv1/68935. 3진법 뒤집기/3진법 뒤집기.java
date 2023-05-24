ec974e89f73001ec72a875ef622b0e4b22b17ae3
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-05-25 01:43:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,11 +2,13 @@ class Solution {
     public int solution(int n) {
         int result = 0;
         StringBuilder sb = new StringBuilder();
-        while (n > 0) {
-            sb.append(n % 3);
+        while (n > 0) { // 3진수로 변환
+            sb.append(n % 3);   
             n /= 3;
         }
         sb.reverse();
+        
+        // (3^0 * 1) + (3^1 * 2) + (3^2 * 3) + (3^3 * 3) 
         for (int i = 0; i < sb.length(); i++) {
             result += Integer.parseInt(sb.substring(i, i + 1)) * Math.pow(3, i);
         }
