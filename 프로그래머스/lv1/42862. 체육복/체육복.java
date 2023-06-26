349d7c8c952603825642ff7b8610ed98c7741589
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-06-26 13:23:51 +0900

    [level 1] Title: 체육복, Time: 0.38 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Solution {
+    /**
+     * n: 전체학생수
+     * lost: 체육복을 도난당한 학생들의 번호
+     * reverse: 여벌 체육복을 가져온 학생들의 번호
+     */
+   public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        for (int i = 0; i < lost.length; i++) {
+            int lostNum = lost[i];
+            for (int j = 0; j < reserve.length; j++) {
+                if (lostNum == reserve[j]) {
+                    answer++;
+                    reserve[j] = -10;
+                    lost[i] = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            int lostNum = lost[i];
+            for (int j = 0; j < reserve.length; j++) {
+                if (lostNum + 1 == reserve[j] || lostNum - 1 == reserve[j]) {
+                    answer++;
+                    reserve[j] = -10;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
