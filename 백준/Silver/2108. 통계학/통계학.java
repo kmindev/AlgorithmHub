4cd992e6eb31ceb1854016fd8e5927f245288012
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-06-30 01:28:10 +0900

    [Silver III] Title: 통계학, Time: 872 ms, Memory: 68292 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> inputs = new LinkedList<>();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int sum = 0; //
+        int freq = 0; // 최빈값
+        int[] numCount = new int[8001]; // 양수 빈도수를 카운트 하기 위한 배열
+        int maxCount = Integer.MIN_VALUE;
+        List<Integer> maxCountIndex = new LinkedList<>();
+
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            sum += input;
+            min = Math.min(input, min);
+            max = Math.max(input, max);
+            inputs.add(input);
+            numCount[input + 4000]++;
+        }
+
+        Collections.sort(inputs);
+
+//        for (int i = 0; i < inputs.size(); i++) { // 빈도를 구하기 위함.
+//            /**
+//             * 음수: 0 ~ 3999,       -4000 = 0 ,    -1 = 3999
+//             * 0 =  4000
+//             * 양수: 4001 ~ 8000    1 = 4001,   4000 = 8000
+//             */
+//            numCount[inputs.get(i) + 4000]++;
+//        }
+
+        for (int i = 0; i < numCount.length; i++) {
+            maxCount = Math.max(maxCount, numCount[i]); // 최대 빈도수를 구함.
+        }
+
+        for (int i = 0; i < numCount.length; i++) {
+            if (maxCount == numCount[i]) {  // 빈도수가 같은 index 를 리스트에 저장
+                maxCountIndex.add(i);
+            }
+        }
+
+        Collections.sort(maxCountIndex);
+
+        if (maxCountIndex.size() > 1) { // 최대 빈도수가 2개 이상일 때
+            freq = maxCountIndex.get(1) - 4000; // 최대 빈도수 중 두번째로 작은 값.
+        } else { // 최대 빈도수가 1개 일 때
+            freq = maxCountIndex.get(0) - 4000; // 최대 빈도수
+        }
+
+
+        float avg = (float) sum / N;
+        int avgRes = avg > 0 ? (int) (avg + 0.5) : (int) (avg - 0.5); // 평균이 양수와 음수 각각 반올림 처리
+
+        System.out.println(avgRes);
+        System.out.println(inputs.get(N / 2));
+        System.out.println(freq);
+        System.out.println(N == 1 ? 0 : (max - min)); // 입력받은 수가 1개이면 최대와 최소는 같음.
+    }
+}
