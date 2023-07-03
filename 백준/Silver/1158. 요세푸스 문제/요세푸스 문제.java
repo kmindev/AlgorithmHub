e1c81e211cf171ed247cbaa34ab5357c49cc0860
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-03 23:45:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 468 ms, Memory: 15460 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,18 +11,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        LinkedList<Integer> numbers = new LinkedList<>();
+        Deque<Integer> numbers = new ArrayDeque<>();
         int count = 1;
 
         for (int i = 1; i <= N; i++)
-            numbers.add(i);
+            numbers.offer(i);
 
         System.out.print("<");
         while (numbers.size() != 1) { // 제거한 번호가 N이랑 같으면 모두 제거한거기 때문에 종료
             if (count % K == 0) { // k번쨰 일 때 제거한다.
-                System.out.print(numbers.remove() + ", "); 
+                System.out.print(numbers.poll() + ", ");
             } else { // 원 모양이기 때문에 삭제 -> 추가
-                numbers.add(numbers.remove());
+                numbers.offerLast(numbers.poll());
             }
             count++;
         }
