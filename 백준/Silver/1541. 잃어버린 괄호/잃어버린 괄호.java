d75ba09155d6e0668bcc3a6b81f48f3e3c89fd0d
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-05 23:56:23 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -5,13 +5,13 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // -를 기준으로 식을 분할한다.
         String[] input = br.readLine().split("-");
 
-        int result = findTemp(input[0].split("\\+"));
+        int result = findTemp(input[0].split("\\+")); // 첫번째 식의 결과를 result에 대입
 
         for (int i = 1; i < input.length; i++) {
-            String[] exp = input[i].split("\\+");
-            result -= findTemp(exp);
+            result -= findTemp(input[i].split("\\+")); // 하나의 식(exp)의 결과 temp를 구해서 result에서 뺀다.
         }
 
         System.out.println(result);
