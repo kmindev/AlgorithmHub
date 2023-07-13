ae8f098943d4b6cee3a1b2ec449e9140cf1c5978
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-13 18:23:42 +0900

    [Silver III] Title: 주유소, Time: 248 ms, Memory: 31568 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -8,18 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine()); // 도시의 개수
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
         int[] distance = new int[n - 1]; // 도시 간의 거리
-        int[] oliPrice = new int[n - 1]; // 도시의 기름
+        int[] oliPrice = new int[n - 1]; // 도시의 기름, 마지막 도시 기름 가격은 알 필요가 없기 때문에 저장하지 않는다.
         for (int i = 0; i < distance.length; i++) {
-            distance[i] = Integer.parseInt(st.nextToken());
+            distance[i] = Integer.parseInt(st1.nextToken());
+            oliPrice[i] = Integer.parseInt(st2.nextToken());
         }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n - 1; i++) {
-            oliPrice[i] = Integer.parseInt(st.nextToken());
-        }
-        int minPrice = oliPrice[0];
-        int result = minPrice * distance[0];
+
+        int minPrice = oliPrice[0]; // 리터당 가격은 1 ~ 1,000,000,000 이기 때문에 int
+        long result = minPrice * distance[0]; // 도시의 개수(2 ~100,000) , 도시간의 거리 (1 ~ 1,000,000,000)이기 떄문에 long
+
         for (int i = 1; i < oliPrice.length; i++) {
             if (minPrice > oliPrice[i]) {
                 minPrice = oliPrice[i];
