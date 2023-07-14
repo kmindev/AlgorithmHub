33196a1aa81255d1f5d2e8ae8f172ffe2bea1131
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-14 17:56:23 +0900

    [Gold V] Title: 동전 2, Time: 92 ms, Memory: 11752 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 화폐의 가치 N개
+        int m = Integer.parseInt(st.nextToken()); // 만들고자 하는 금액
+
+        int[] coinTypes = new int[n];
+        int[] dp = new int[m + 1];
+
+        for (int i = 0; i < n; i++)
+            coinTypes[i] = Integer.parseInt(br.readLine());
+
+        Arrays.fill(dp, 100000);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coinTypes[i]; j <= m; j++) {
+                if (dp[j - coinTypes[i]] != 100000)  // 화폐 종류와 금액이 나누어 떨어질 때
+                    dp[j] = Math.min(dp[j], dp[j - coinTypes[i]] + 1);
+            }
+        }
+
+        System.out.println(dp[m] != 100000 ? dp[m] : -1);
+
+    }
+}
