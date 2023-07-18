20fa8114e64a8a98f751019cafdf19f139861339
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-18 13:52:13 +0900

    [level 2] Title: 올바른 괄호, Time: 18.26 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Deque<String> q = new ArrayDeque<>();
+        
+        for(int i=0; i<s.length(); i++) {
+            String s1 = s.substring(i, i+1);
+            if(s1.equals("(")) {
+                q.offer(s1);
+            } else {
+                if(q.poll()==null)
+                    return false;
+            }
+        }
+
+        return q.size() == 0;
+    }
+}
