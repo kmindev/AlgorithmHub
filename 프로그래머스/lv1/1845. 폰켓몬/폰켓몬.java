aa4c49270d0b4753b2698c8c9886cbcb3a2da8b5
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-20 16:25:58 +0900

    [level 1] Title: 폰켓몬, Time: 3.68 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,14 +1,18 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int selAmt = nums.length / 2;
+        // 1. Set에 nums를 저장한다.
+        HashSet<Integer> numSet = new HashSet<>();
         
-        HashSet<Integer> set = new HashSet<>();
         for(int num : nums) {
-            set.add(num);
+            numSet.add(num);
         }
+    
+        // 2. 선택할 수 있는 수만큼 set에서 선택할 수 있다.
+        int choice = nums.length / 2;
+        int size = numSet.size();
         
-        return set.size() > selAmt ? selAmt : set.size();
+        return size < choice ? size : choice;
     }
 }
