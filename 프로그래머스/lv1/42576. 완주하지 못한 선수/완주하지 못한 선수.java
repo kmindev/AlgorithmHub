af19cf6096ee28b71f4ebc07775041ab69d08e74
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-07-20 16:37:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 70.08 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,18 +1,28 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        int nCompIndex = participant.length - 1;
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        // 1. 참가자를 map에 저장한다. 동명이인의 경우 value를 1씩 증가시킨다.
+        HashMap<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                nCompIndex = i;
-                break;
-            }
+        for(String part : participant) {
+            map.put(part, map.getOrDefault(part, 0) + 1);
         }
-
-        return participant[nCompIndex];
+        
+        // 2. 완주자를 map에 저장, map에 존재할 경우 value를 1 감소해준다.
+        for(String comp : completion) {
+            map.put(comp, map.getOrDefault(comp, 0) - 1);
+        }
+        
+        // 3. value가 1 이상인 참가자는 완주를 못했기 때문에 return => 한명의 선수만 완주하지 못했기 때문에 가능
+        for(String part : map.keySet()) {
+            if (map.get(part) > 0)
+                return part;
+        }
+        
+        return "";
+        
     }
 }
