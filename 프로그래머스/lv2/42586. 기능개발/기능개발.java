5e6d239c22d020e5aafd6accc5105dd4eb11b426
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-08-12 13:38:02 +0900

    [level 2] Title: 기능개발, Time: 2.06 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,30 +1,27 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(int[] progresses, int[] speeds) {
-        int[] days = new int[progresses.length];
-        Deque<Integer> q = new ArrayDeque<>();
-        List<Integer> result = new ArrayList<>();
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new ArrayList<>();
         
         for(int i=0; i<progresses.length; i++) {
-            int p = 100 - progresses[i]; // 남은 작업량
-            int compDay = p % speeds[i] > 0 ? p/speeds[i] + 1 : p/speeds[i]; // 배포 가능 일
-            days[i] = compDay;
-        }
-        
-        q.offer(days[0]);
-        
-        for(int i=1; i<days.length; i++) {
-            if(q.peek() >= days[i]) {
-                q.offer(days[i]);
-            } else {
-                result.add(q.size());
-                q.clear();
-                q.offer(days[i]);
+            double days = (100 - progresses[i]) / (double)speeds[i];
+            int day = (int) Math.ceil(days);
+            
+            int j = i + 1;
+            
+            for(; j<progresses.length; j++) {
+                double jDays = (100 - progresses[j]) / (double)speeds[j];
+                int jDay = (int) Math.ceil(jDays);
+                
+                if(jDay > day)
+                    break;
             }
+            
+            answer.add(j - i);
+            i = j - 1;
         }
-        result.add(q.size());
-     
-        return result;
+        
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
     }
 }
