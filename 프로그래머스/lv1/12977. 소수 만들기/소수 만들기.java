6c3d6f3e8313f6196552d66cb9f1afef1a4539aa
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-08-21 20:12:00 +0900

    [level 1] Title: 소수 만들기, Time: 0.05 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i + 1; j<nums.length; j++) {
+                for(int k= j + 1; k < nums.length; k++) {
+                    if(isPrime(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isPrime(int n) {
+        if(n == 0 || n == 1) {
+            return false;
+        }
+        
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
