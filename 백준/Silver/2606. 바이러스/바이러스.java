34915e5cf8478b46d25537c31a976d74e0a7d3e7
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-08-27 16:04:18 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static int COUNT = 0;
+class Main {
+    static int answer = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int amount = Integer.parseInt(br.readLine()); // 컴퓨터 수
-        int con = Integer.parseInt(br.readLine()); // 연결된 컴퓨터 쌍의 수
-        ArrayList<ArrayList<Integer>> networkMap = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i < amount + 1; i++)
-            networkMap.add(new ArrayList<>());
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < con; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        boolean[] visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            networkMap.get(x).add(y);
-            networkMap.get(y).add(x);
+            graph.get(x).add(y);
+            graph.get(y).add(x);
         }
 
-        boolean[] visited = new boolean[amount + 1];
+        dfs(graph, visited, 1);
 
-        dfs(1, visited, networkMap);
-        System.out.println(COUNT);
+        System.out.println(answer);
     }
 
-    private static void dfs(int start, boolean[] visited, ArrayList<ArrayList<Integer>> networkMap) {
-        visited[start] = true;
+    private static void dfs(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int node) {
+        answer++;
+        visited[node] = true;
 
-        for(int node : networkMap.get(start)) {
-            if(!visited[node]) {
-                COUNT++;
-                dfs(node, visited, networkMap);
+        for (int i = 0; i < graph.get(node).size(); i++) {
+            if(!visited[graph.get(node).get(i)]) {
+                visited[graph.get(node).get(i)] = true;
+                dfs(graph, visited, graph.get(node).get(i));
             }
         }
     }
