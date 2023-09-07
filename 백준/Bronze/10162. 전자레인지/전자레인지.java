414465c87b831ff46659787781557d1a511d20f7
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-09-07 22:18:55 +0900

    [Bronze III] Title: 전자레인지, Time: 76 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 1. 입력 받기
+        int t = Integer.parseInt(br.readLine());
+        int[] times = {3000, 60, 10};
+
+        // 2. T초를 맞출 수 없을 때
+        if (t % 10 != 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        StringBuilder answer = new StringBuilder();
+
+        // 3. times 반복문 돌면서 answer에 결과 저장
+        for (int time : times) {
+            if (t >= time) {
+                answer.append(t / time).append(" ");
+                t %= time;
+            } else {
+                answer.append(0).append(" ");
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
