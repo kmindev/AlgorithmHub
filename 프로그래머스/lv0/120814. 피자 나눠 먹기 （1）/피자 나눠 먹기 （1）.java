0c7cc3d831e977905e3dce126265ac5428e53412
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-10-02 22:57:45 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        if(n % 7 == 0) 
-            answer = n / 7;
-        else 
-            answer = n / 7 + 1; 
-        
-        return answer;
+        return (int) ((n / 7.0) + 0.9);
     }
 }
