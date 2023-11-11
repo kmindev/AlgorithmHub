0f57237a1216de9bb8cc52fa9e30a1d4cac703e8
KANGKYEONGMIN <97210232+kmindev@users.noreply.github.com>
2023-11-11 21:42:45 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int price) {
-        double discount = 0.0;
         
         if(price >= 500000) {
-            discount = price * 0.2;
+            price *= 0.8;
         } else if(price >= 300000) {
-            discount = price * 0.1;
+            price *= 0.9;
         } else if(price >= 100000) {
-            discount = price * 0.05 ;
+            price *= 0.95 ;
         }
         
-        return (int) (price - discount);
+        return price;
     }
 }
