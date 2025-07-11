56bd2616974db87b992f43f50d9e347884b493c3
Kang KyeongMin <kangkm0324@gmail.com>
2025-07-11 15:26:13 +0900

    [Silver V] Title: 색종이, Time: 64 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 1. 입력 받는다.
         int n = Integer.parseInt(br.readLine());
         boolean[][] arr = new boolean[100][100];
+        int result = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
@@ -17,16 +17,10 @@ public class Main {
 
             for (int j = w; j < w + 10; j++) {
                 for (int k = h; k < h + 10; k++) {
-                    arr[j][k] = true;
-                }
-            }
-        }
-
-        int result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j]) {
-                    result += 1;
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        result += 1;
+                    }
                 }
             }
         }
