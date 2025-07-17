f94e0aa7fc48841d2b16087b796b651f46722c6a
Kang KyeongMin <kangkm0324@gmail.com>
2025-07-17 11:39:21 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,39 +1,44 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
-//Scanner 대신 BufferedReader, BufferedWriter 사용
 
 public class Main {
-	public static void main(String[] args) throws IOException  {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		int count = 0;
-		
-		for(int i=1; i<=n; i++) {
-			boolean check = hansu(i);
-			if(check == true)
-				count++;
-		}
-		System.out.println(count);
-	}
-	public static boolean hansu(int x) {
-		boolean chek = false;
-		if(x<100) //99까지는 무조건 한수
-			return true;
-		else { //100이상
-			int a = x%10; //첫째자리
-			int b = (x/10)%10; //둘째자리
-			int c = x/100; //셋째자리
-			
-			if((c-b) == (b-a))
-				return true;
-			else
-				return false;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N < 100) {
+            System.out.println(N);
+            return;
+        }
+
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            if (isHansu(i)) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static boolean isHansu(int x) {
+        // x는 1000이하 작으므로 1 ~ 999
+
+        // 1000은 한수 x
+        if (x == 1000) {
+            return false;
+        }
+
+        // 1 ~ 99는 한수
+        if (x < 100) {
+            return true;
+        }
+
+        // 100 ~ 999
+        int a = x % 10; // 1의 자리
+        int b = (x / 10) % 10; // 10의 자리
+        int c = (x / 100) % 10; // 100의 자리
+
+        return (b - a) == (c - b);
+    }
 }
