bfba005e4c96440ba986b48a5dccbf18db4deb7e
Kang KyeongMin <kangkm0324@gmail.com>
2025-07-20 16:15:09 +0900

    [Silver III] Title: N과 M (5), Time: 220 ms, Memory: 31676 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder out;
+    static int N;
+    static int M;
+    static int[] numbers;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        out = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        numbers = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        int[] selectedNumbers = new int[M];
+        boolean[] visited = new boolean[N];
+        back(visited, selectedNumbers, 0);
+
+        bw.write(out.toString());
+        bw.flush();
+    }
+
+    private static void back(boolean[] visited, int[] selectedNumbers, int depth) {
+        if (depth == M) {
+            for (int num : selectedNumbers) {
+                out.append(num).append(" ");
+            }
+            out.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                selectedNumbers[depth] = numbers[i];
+                back(visited, selectedNumbers, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
