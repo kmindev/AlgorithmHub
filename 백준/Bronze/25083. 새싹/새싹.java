1c38a66d0828f7640e1be66d6372286d2630d0e7
Kang KyeongMin <kangkm0324@gmail.com>
2025-07-29 14:28:35 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11356 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String s = "         ,r'\"7\n"
+                + "r`-_   ,'  ,/\n"
+                + " \\. \". L_r'\n"
+                + "   `~\\/\n"
+                + "      |\n"
+                + "      |";
+        System.out.println(s);
+    }
+}
