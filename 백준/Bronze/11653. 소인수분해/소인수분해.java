268675557b491c1147453483f6c08aa7be292b50
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-01 17:16:53 +0900

    [Bronze I] Title: 소인수분해, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -7,14 +7,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-        while (N > 1) {
-            for (int i = 2; i <= N; i++) {
-                if (N % i == 0) {
-                    N /= i;
-                    System.out.println(i);
-                    break;
-                }
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            while (N % i == 0) {
+                System.out.println(i);
+                N /= i;
             }
+        }
+        if (N != 1) {
+            System.out.println(N);
         }
     }
 }
