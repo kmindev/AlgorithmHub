8ad40cc39a92e08669f8f6dce0443b52ec095242
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-01 15:33:49 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 100 ms, Memory: 13560 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] a;
+    static int[][] b;
+    static int[][] result;
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        a = new int[N][M];
+        b = new int[N][M];
+        StringBuilder out = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                b[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                out.append(a[i][j] + b[i][j]).append(" ");
+            }
+            out.append("\n");
+        }
+
+        System.out.println(out);
+    }
+}
