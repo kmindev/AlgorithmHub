475cf4cd2cdac05941a39b3152683bad44136209
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-03 17:37:47 +0900

    [Bronze III] Title: 대지, Time: 200 ms, Memory: 25712 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int xMin = Integer.MAX_VALUE;
+        int xMax = Integer.MIN_VALUE;
+        int yMin = Integer.MAX_VALUE;
+        int yMax = Integer.MIN_VALUE;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            xMin = Math.min(x, xMin);
+            xMax = Math.max(x, xMax);
+            yMin = Math.min(y, yMin);
+            yMax = Math.max(y, yMax);
+        }
+        System.out.println((xMax - xMin) * (yMax - yMin));
+    }
+}
