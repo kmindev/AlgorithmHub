a7c7240ccdcb386ae19d9f692cb4ebd9a6a7230e
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-03 23:55:23 +0900

    [Bronze III] Title: 세 막대, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        if (arr[0] + arr[1] <= arr[2]) {
+            arr[2] = arr[0] + arr[1] - 1;
+        }
+
+        System.out.println(arr[0] + arr[1] + arr[2]);
+    }
+}
