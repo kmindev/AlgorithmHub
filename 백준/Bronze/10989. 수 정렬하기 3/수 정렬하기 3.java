c9328e1691356944d4e6e2bc759d8ab231d2474f
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-09 09:02:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2532 ms, Memory: 343552 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for (int num : arr) {
+            bw.write(num + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}
