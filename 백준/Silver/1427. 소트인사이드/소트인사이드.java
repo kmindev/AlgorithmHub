925dbf6b0cb3b73649164efc0491254a8309ef65
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-16 21:32:44 +0900

    [Silver V] Title: 소트인사이드, Time: 68 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -3,25 +3,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
+        int[] arr = new int[10];
         String N = br.readLine();
-        List<Integer> list = Arrays.stream(N.split(""))
-                .map(Integer::parseInt)
-                .sorted(Comparator.reverseOrder())
-                .collect(Collectors.toList());
-
-        for (Integer num : list) {
-            bw.write(num + "");
+        for (int i = 0; i < N.length(); i++) {
+            int num = N.charAt(i) - '0';
+            arr[num]++;
         }
+
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j < arr[i]; j++) {
+                bw.write(i + "");
+            }
+        }
+
         bw.flush();
         bw.close();
     }
