718436ecfcd46f15fe97b73a6f8b8515f9153a41
Kang KyeongMin <kangkm0324@gmail.com>
2025-08-26 15:58:41 +0900

    [Silver I] Title: 강아지는 많을수록 좋다, Time: 148 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.java
+++ 백준/Silver/27971. 강아지는 많을수록 좋다/강아지는 많을수록 좋다.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,17 +7,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // 목표 마리 수
+        int M = Integer.parseInt(st.nextToken()); // 닫힌 구간 수
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
+
+        // A < B로 통일
         if (A > B) {
-            int swap = A;
+            int temp = A;
             A = B;
-            B = swap;
+            B = temp;
         }
 
         int[] dp = new int[N + 1];
+
+        // 닫힌 구간 -1 처리
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
@@ -33,25 +33,23 @@ public class Main {
 
         for (int i = 1; i <= N; i++) {
             if (dp[i] == -1) {
-                continue;
+                continue; // 닫힌 구간
             }
 
             if (i < A) {
                 dp[i] = -1;
             } else if (i < B) {
-                if (dp[i - A] == -1) {
-                    dp[i] = -1;
-                } else {
-                    dp[i] = dp[i - A] + 1;
-                }
-            } else if (dp[i - A] == -1 && dp[i - B] == -1) {
-                dp[i] = -1;
-            } else if (dp[i - A] == -1) {
-                dp[i] = dp[i - B] + 1;
-            } else if (dp[i - B] == -1) {
-                dp[i] = dp[i - A] + 1;
+                dp[i] = (dp[i - A] == -1) ? -1 : dp[i - A] + 1;
             } else {
-                dp[i] = Math.min(dp[i - A], dp[i - B]) + 1;
+                if (dp[i - A] == -1 && dp[i - B] == -1) {
+                    dp[i] = -1;
+                } else if (dp[i - A] == -1) {
+                    dp[i] = dp[i - B] + 1;
+                } else if (dp[i - B] == -1) {
+                    dp[i] = dp[i - A] + 1;
+                } else {
+                    dp[i] = Math.min(dp[i - A], dp[i - B]) + 1;
+                }
             }
         }
 
