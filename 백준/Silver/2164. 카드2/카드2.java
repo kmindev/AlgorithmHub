ecdd68e40cf317df9f3b259ba7ac9929aad3ec57
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-03 11:12:08 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 43344 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,25 +1,34 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
 
-class Main {
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new LinkedList<>();
 
-        int n = Integer.parseInt(br.readLine());
-
-        Queue<Integer> q = new ArrayDeque<>();
-
-        for (int i = 1; i <= n; i++) {
-            q.add(i);
+        for (int i = 1; i <= N; i++) {
+            dq.add(i);
         }
 
-        while (q.size() != 1) {
-            q.poll();
-
-            int num = q.poll();
-            q.add(num);
+        while (dq.size() > 1) {
+            dq.pollFirst();
+            Integer firstNum = dq.pollFirst();
+            dq.addLast(firstNum);
         }
 
-        System.out.println(q.poll());
+        bw.write(dq.pollLast() + "");
+
+        bw.flush();
+        bw.close();
     }
 }
