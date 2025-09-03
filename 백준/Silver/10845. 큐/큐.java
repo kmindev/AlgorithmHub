5934973f23453c143a308d192ad2e870c37b53db
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-03 11:39:41 +0900

    [Silver IV] Title: 큐, Time: 132 ms, Memory: 17196 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,44 +1,64 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> queue = new LinkedList<>();
-        int N = Integer.parseInt(br.readLine());
-        int lastNum = 0;
+        N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new LinkedList<>();
 
-        for(int i=0; i<N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            while (st.hasMoreTokens()) {
-                String command = st.nextToken();
-
-                if(command.equals("push")) {
-                    int pushNum = Integer.parseInt(st.nextToken());
-                    queue.offer(pushNum);
-                    lastNum = pushNum;
-                } else if (command.equals("pop")) {
-                    Integer pollRes = queue.poll();
-                    int res = pollRes == null ? -1 : pollRes;
-                    System.out.println(res);
-                } else if (command.equals("size")) {
-                    System.out.println(queue.size());
-                } else if (command.equals("empty")) {
-                    int res = queue.isEmpty() ? 1 : 0;
-                    System.out.println(res);
-                } else if (command.equals("front")) {
-                    Integer peekRes = queue.peek();
-                    int res = peekRes == null ? -1 : peekRes;
-                    System.out.println(res);
-                } else if (command.equals("back")) {
-                    int res = queue.isEmpty() ? -1 : lastNum;
-                    System.out.println(res);
-                }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            switch (command) {
+                case "push":
+                    int num = Integer.parseInt(st.nextToken());
+                    dq.offer(num);
+                    break;
+                case "pop":
+                    Integer popNum = dq.poll();
+                    if (popNum == null) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(popNum + "\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(dq.size() + "\n");
+                    break;
+                case "empty":
+                    bw.write(dq.isEmpty() ? "1\n" : "0\n");
+                    break;
+                case "front":
+                    Integer frontNum = dq.peekFirst();
+                    if (frontNum == null) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(frontNum + "\n");
+                    }
+                    break;
+                case "back":
+                    Integer backNum = dq.peekLast();
+                    if (backNum == null) {
+                        bw.write("-1\n");
+                    } else {
+                        bw.write(backNum + "\n");
+                    }
+                    break;
             }
         }
+
+        bw.flush();
+        bw.close();
     }
 }
