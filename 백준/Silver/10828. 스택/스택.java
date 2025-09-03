b2fbddacc24d674e7613b6d1b679deb3f28cac7a
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-03 10:56:26 +0900

    [Silver IV] Title: 스택, Time: 132 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,35 +1,51 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
         Stack<Integer> stack = new Stack<>();
-
-        int N = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            String token = st.nextToken();
-
-            if (token.equals("push")) {
-                stack.push(Integer.parseInt(st.nextToken()));
-            } else if (token.equals("pop")) {
-                int result = stack.empty() ? -1 : stack.pop();
-                System.out.println(result);
-            } else if (token.equals("size")) {
-                System.out.println(stack.size());
-            } else if (token.equals("empty")) {
-                int result = stack.empty() ?  1 : 0;
-                System.out.println(result);
-            } else if (token.equals("top")) {
-                int result = stack.empty() ? -1 : stack.peek();
-                System.out.println(result);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                stack.push(num);
+            } else if (command.equals("pop")) {
+                if (stack.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(stack.pop() + "\n");
+                }
+            } else if (command.equals("size")) {
+                bw.write(stack.size() + "\n");
+            } else if (command.equals("empty")) {
+                if (stack.isEmpty()) {
+                    bw.write("1\n");
+                } else {
+                    bw.write("0\n");
+                }
+            } else if (command.equals("top")) {
+                if (stack.isEmpty()) {
+                    bw.write("-1\n");
+                } else {
+                    bw.write(stack.peek() + "\n");
+                }
             }
         }
 
+        bw.flush();
+        bw.close();
     }
 }
