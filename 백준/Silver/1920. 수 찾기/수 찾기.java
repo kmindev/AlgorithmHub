f501f41d703bbabe1c03f85af44ce95c0266dc59
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-04 11:43:04 +0900

    [Silver IV] Title: 수 찾기, Time: 456 ms, Memory: 55532 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,28 +1,40 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] n = new int[N];
-        for (int i = 0; i < N; i++)
-            n[i] = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(br.readLine());
-        Arrays.sort(n);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        st = new StringTokenizer(br.readLine(), " ");
-        
-        for (int i = 0; i < M; i++) {
-            int result = Arrays.binarySearch(n, Integer.parseInt(st.nextToken()));
-            if(result >= 0 )
-                System.out.println(1);
-            else
-                System.out.println(0);
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
+
+        M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            int result = Arrays.binarySearch(arr, x);
+
+            if (result >= 0) {
+                bw.write(1 + "\n");
+            } else {
+                bw.write(0 + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
