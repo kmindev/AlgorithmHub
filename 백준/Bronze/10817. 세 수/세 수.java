b732af34a69fd8188abfcff30419b06260eb11a5
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-05 21:39:38 +0900

    [Bronze IV] Title: 세 수, Time: 64 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        bw.write(arr[1] + "\n");
+        bw.flush();
+        bw.close();
+    }
+
+}
