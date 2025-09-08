9331ef055118dab779bb0b256b5257cd661bcf96
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-08 16:46:20 +0900

    [Silver II] Title: DFS와 BFS, Time: 164 ms, Memory: 18028 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,99 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N; // 정점의 개수
+    static int M; // 간선의 개수
+    static int V; // 시작 번호
+
+    static List<List<Node>> graph = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken()); // 정점의 개수
-        int M = Integer.parseInt(st.nextToken()); // 간선의 개수
-        int V = Integer.parseInt(st.nextToken()); // 시작 노드
-        boolean[] visited = new boolean[N + 1];
-        for (int i = 0; i < N + 1; i++)
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i <= N; i++) {
             graph.add(new ArrayList<>());
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph.get(x).add(y);
-            graph.get(y).add(x);
+
+            graph.get(x).add(new Node(y));
+            graph.get(y).add(new Node(x));
         }
 
-        for (ArrayList<Integer> arr : graph)
-            Collections.sort(arr);
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
 
-        dfs(V, visited);
-        System.out.println();
+        boolean[] visited = new boolean[N + 1];
+        dfs(visited, V);
+
+        bw.write("\n");
+
         visited = new boolean[N + 1];
-        bfs(V, visited);
+        bfs(visited, V);
+
+        bw.flush();
+        bw.close();
     }
 
-    private static void dfs(int V, boolean[] visited) {
-        visited[V] = true; // 방문처리
-        System.out.print(V + " ");
-        for (int node : graph.get(V)) { // 현재 노드와 연결된 다른 노드를 방문하지 않았으면 재귀적으로 방문
-            if (!visited[node]) {
-                dfs(node, visited);
-            }
-        }
-    }
+    static void bfs(boolean[] visited, int v) throws IOException {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(v);
+        visited[v] = true;
 
-    private static void bfs(int V, boolean[] visited) {
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.offer(V);
-        visited[V] = true; // 시작 노드 방문 처리
-        while (!deque.isEmpty()) { 
-            int x = deque.poll(); // 큐에서 하나 뽑아서 출력
-            System.out.print(x + " ");
-            for (int node : graph.get(x)) { // depth 가 같은걸 탐색
-                if (!visited[node]) {
-                    deque.offer(node);
-                    visited[node] = true;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            bw.write(cur + " ");
+
+            for (Node next : graph.get(cur)) {
+                if (!visited[next.num]) {
+                    q.offer(next.num);
+                    visited[next.num] = true;
                 }
             }
         }
     }
 
+    static void dfs(boolean[] visited, int cur) throws IOException {
+        visited[cur] = true;
+        bw.write(cur + " ");
+        for (Node next : graph.get(cur)) {
+            if (!visited[next.num]) {
+                dfs(visited, next.num);
+            }
+        }
+    }
+
+    static class Node implements Comparable<Node> {
+        int num;
+
+        Node(int num) {
+            this.num = num;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.num - o.num;
+        }
+    }
 }
