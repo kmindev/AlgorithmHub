65be25d39159f8ec0e2ce54267c7c82b989b8d2d
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-10 15:01:01 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 12336 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,83 +1,68 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int[][] map;
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+    static int M;
+    static int[][] map;
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         map = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            String input = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = input.charAt(j) - '0';
+                int num = line.charAt(j) - '0';
+                map[i][j] = num;
             }
         }
-        System.out.println(bfs(map, 0, 0));
+
+        bfs(0, 0);
+
+        bw.write(map[N - 1][M - 1] + "\n");
+        bw.flush();
+        bw.close();
     }
 
-    private static int bfs(int[][] map, int x, int y) {
-        Deque<Node> deque = new ArrayDeque<>();
+    static void bfs(int x, int y) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
 
-        deque.offer(new Node(x, y));
-
-        while (!deque.isEmpty()) {
-            Node node = deque.poll();
-            x = node.getX();
-            y = node.getY();
-
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
             for (int i = 0; i < 4; i++) {
-                // 다음 이동할 위치
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                // 범위에 벗어날 때
-                if (nx < 0 || nx >= map.length || ny < 0 || ny >= map[x].length)
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M || map[nx][ny] != 1) {
                     continue;
-
-                // 벽 인 경우
-                if (map[nx][ny] == 0)
-                    continue;
-
-                else if (map[nx][ny] == 1) { // 벽이 아니고 지나간 적이 없을 때
-                    map[nx][ny] = map[x][y] + 1; // 거리 누적
-                    deque.offer(new Node(nx, ny));
                 }
+                map[nx][ny] = map[cur.x][cur.y] + 1;
+                q.offer(new Point(nx, ny));
             }
         }
-
-        return map[map.length - 1][map[x].length - 1];
-
     }
 
-    public static class Node {
-        private int x;
-        private int y;
+    static class Point {
+        int x;
+        int y;
 
-        public Node(int x, int y) {
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
     }
 }
