628b8fe59592e089627dfe36638a275817cf4589
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-12 14:59:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 11744 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,89 +1,86 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Main {
-    public static int[] dx = {-1, 1, 0, 0};
-    public static int[] dy = {0, 0, -1, 1};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    static int N;
+    static int[][] map;
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        int n = Integer.valueOf(br.readLine());
+        map = new int[N][N];
 
-        int[][] map = new int[n][n];
-        int count = 0;
-        ArrayList<Integer> resList = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < n; j++) {
-                map[i][j] = s.charAt(j) - '0';
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = input.charAt(j) - '0';
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        int result = 0;
+        List<Integer> counts = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 if (map[i][j] == 1) {
-                    resList.add(bfs(i, j, map, 0));
-                    count++;
+                    result++;
+                    int count = bfs(i, j);
+                    counts.add(count);
                 }
             }
         }
-        Collections.sort(resList);
-
-        System.out.println(count);
-        for (int res : resList) {
-            System.out.println(res);
+        
+        Collections.sort(counts);
+        bw.write(result + "\n");
+        for (int count : counts) {
+            bw.write(count + "\n");
         }
+
+        bw.flush();
+        bw.close();
     }
 
-    private static int bfs(int x, int y, int[][] map, int count) {
-        Deque<Node> q = new ArrayDeque<>();
-        q.offer(new Node(x, y));
-        map[x][y] = 0;
+    static int bfs(int x, int y) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
+        map[x][y] = 2; // 방문 처리
+        int count = 0;
 
         while (!q.isEmpty()) {
+            Point cur = q.poll();
             count++;
-            Node node = q.poll();
-            x = node.getX();
-            y = node.getY();
-
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx < 0 || ny < 0 || nx > map.length - 1 || ny > map[x].length - 1 || map[nx][ny] == 0)
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] != 1) {
                     continue;
-
-                if (map[nx][ny] == 1) {
-                    q.offer(new Node(nx, ny));
-                    map[nx][ny] = 0;
                 }
+                map[nx][ny] = 2;
+                q.offer(new Point(nx, ny));
             }
         }
         return count;
     }
 
+    static class Point {
+        int x;
+        int y;
 
-    public static class Node {
-        private int x;
-        private int y;
-
-        public Node(int x, int y) {
+        Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
-
-        public int getY() {
-            return y;
-        }
-
-        public int getX() {
-            return x;
-        }
     }
-
 }
