8cd53799cb66d674df11f3b55c00cbc2c066e888
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-12 15:09:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,9 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +15,7 @@ public class Main {
     static int[][] map;
     static int[] dx = {0, -1, 0, 1};
     static int[] dy = {-1, 0, 1, 0};
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
@@ -36,12 +35,13 @@ public class Main {
             for (int j = 0; j < N; j++) {
                 if (map[i][j] == 1) {
                     result++;
-                    int count = bfs(i, j);
+                    dfs(i, j);
                     counts.add(count);
+                    count = 0;
                 }
             }
         }
-        
+
         Collections.sort(counts);
         bw.write(result + "\n");
         for (int count : counts) {
@@ -52,35 +52,17 @@ public class Main {
         bw.close();
     }
 
-    static int bfs(int x, int y) {
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(x, y));
-        map[x][y] = 2; // 방문 처리
-        int count = 0;
-
-        while (!q.isEmpty()) {
-            Point cur = q.poll();
-            count++;
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] != 1) {
-                    continue;
-                }
-                map[nx][ny] = 2;
-                q.offer(new Point(nx, ny));
+    static void dfs(int x, int y) {
+        map[x][y] = 2;
+        count++;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N || map[nx][ny] != 1) {
+                continue;
             }
-        }
-        return count;
-    }
-
-    static class Point {
-        int x;
-        int y;
-
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+            dfs(nx, ny);
         }
     }
+
 }
