4b46edc4081e4bbcecab539fe979461ef3abd2da
Kang KyeongMin <kangkm0324@gmail.com>
2025-09-28 16:35:53 +0900

    [Gold III] Title: 택배, Time: 292 ms, Memory: 23028 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -3,10 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,8 +12,8 @@ public class Main {
 
     static int N;
     static int M;
-    static List<List<Point>> graph = new ArrayList<>();
-    static int INF = Integer.MAX_VALUE;
+    static int INF = 200 * 10000;
+    static int[][] map;
     static int[][] result;
 
     public static void main(String[] args) throws IOException {
@@ -24,23 +21,36 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+        map = new int[N + 1][N + 1];
+        result = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(map[i], INF);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                result[i][j] = j;
+            }
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-
-            graph.get(start).add(new Point(end, w));
-            graph.get(end).add(new Point(start, w));
+            int weight = Integer.parseInt(st.nextToken());
+            map[end][start] = map[start][end] = Math.min(map[start][end], weight);
         }
 
-        result = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            dijkstra(i);
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                        result[i][j] = result[i][k];
+                    }
+                }
+            }
         }
 
         for (int i = 1; i <= N; i++) {
@@ -57,40 +67,5 @@ public class Main {
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    static void dijkstra(int start) {
-        int[] dist = new int[N + 1];
-        boolean[] visited = new boolean[N + 1];
-        Arrays.fill(dist, INF);
-        PriorityQueue<Point> pq = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
-        pq.offer(new Point(start, 0));
-        dist[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Point cur = pq.poll();
-            if (visited[cur.x]) {
-                continue;
-            }
-            visited[cur.x] = true;
-
-            for (Point next : graph.get(cur.x)) {
-                if (dist[next.x] > dist[cur.x] + next.w) {
-                    dist[next.x] = dist[cur.x] + next.w;
-                    pq.offer(new Point(next.x, dist[next.x]));
-                    result[next.x][start] = cur.x;
-                }
-            }
-        }
-    }
-
-    static class Point {
-        int x;
-        int w;
-
-        Point(int x, int w) {
-            this.x = x;
-            this.w = w;
-        }
     }
 }
