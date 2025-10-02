29237cb8d4d1498cf55f860565012165117b27c5
Kang KyeongMin <kangkm0324@gmail.com>
2025-10-02 18:26:52 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,18 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<clothes.length; i++) {
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        for(String[] clothe : clothes) {
+            String key = clothe[1];
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
         
+        int answer = 1;
         for(String key : map.keySet()) {
             answer *= map.get(key) + 1;
         }
-        
-        
         
         return answer - 1;
     }
