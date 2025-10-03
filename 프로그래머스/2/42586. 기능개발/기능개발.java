da08293dad5c35920bc7acc44aa4f011765b99f9
Kang KyeongMin <kangkm0324@gmail.com>
2025-10-03 22:32:53 +0900

    [level 2] Title: 기능개발, Time: 3.92 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,25 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> q = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        
         for(int i = 0; i < progresses.length; i++) {
             int remainPercent = 100 - progresses[i]; 
             int completedDay = remainPercent / speeds[i];
             if(remainPercent % speeds[i] != 0) {
                 completedDay++;
             }
+            
+            if(!q.isEmpty() && q.peek() < completedDay) {
+                result.add(q.size());
+                q.clear();
+            }
+            
             q.offer(completedDay);
         }
-        
-        List<Integer> result = new ArrayList<>();
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            int count = 1;
-            while(!q.isEmpty() && q.peek() <= cur) {
-                q.poll();
-                count++;
-            }
-            result.add(count);
-        }
+        result.add(q.size());
         
         return result.stream().mapToInt(i -> i).toArray();
     }
