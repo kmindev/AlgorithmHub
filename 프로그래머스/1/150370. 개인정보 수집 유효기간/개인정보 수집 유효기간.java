c35c651dcef79ba653c2a76a8dd1ccb19ea1020b
Kang KyeongMin <kangkm0324@gmail.com>
2025-10-10 01:06:15 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.62 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -15,20 +15,29 @@ class Solution {
     
     private int[] solve(String today, String[] terms, String[] privacies) {
         parseTerms(terms);
-        DateInfo todayDate = DateInfo.from(today);
+        int todayDate = getDate(today);
         
         List<Integer> answer = new ArrayList<>();
         for(int i = 0; i < privacies.length; i++) {
             StringTokenizer st = new StringTokenizer(privacies[i], " ");
-            DateInfo date = DateInfo.from(st.nextToken());
+            int date = getDate(st.nextToken());
             String type = st.nextToken();
             
             int term = termMap.get(type);
-            if(date.isExpired(todayDate, term)) {
+            
+            if(date + (term  * 28) <= todayDate) {
                 answer.add(i + 1);
             }
         }
         return answer.stream().mapToInt(i -> i).toArray();
+    }
+    
+    private int getDate(String s) {
+        StringTokenizer st = new StringTokenizer(s, ".");
+        int year = Integer.parseInt(st.nextToken());
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+        return (year * 12 * 28) + (month * 28) + day;
     }
     
     private void parseTerms(String[] terms) {
@@ -37,57 +46,4 @@ class Solution {
             termMap.put(token[0], Integer.parseInt(token[1]));
         }
     }
-    
-    static class DateInfo {
-        int year;
-        int month;
-        int day;
-        
-        public static DateInfo from(String s) {
-            StringTokenizer st = new StringTokenizer(s, ".");
-            DateInfo dateInfo = new DateInfo();
-            dateInfo.year = Integer.parseInt(st.nextToken());
-            dateInfo.month = Integer.parseInt(st.nextToken());
-            dateInfo.day = Integer.parseInt(st.nextToken());
-            return dateInfo;
-        }
-        
-        public boolean isExpired(DateInfo todayDate, int term) {
-            int expiredYear = this.year;
-            int expiredMonth = this.month + term;
-            int expiredDay = this.day - 1;
-            
-            if(expiredDay == 0) {
-                expiredDay = 28;
-                expiredMonth -= 1;
-            }
-            
-            if(expiredMonth > 12) {
-                expiredYear += (expiredMonth - 1) / 12;
-                expiredMonth = (expiredMonth - 1) % 12 + 1;
-            }
-            return isExpired(todayDate, expiredYear, expiredMonth, expiredDay);
-        }
-        
-        private boolean isExpired(DateInfo todayDate, int expiredYear, int expiredMonth, int expiredDay) {
-            if(expiredYear < todayDate.year) {
-                return true;
-            } else if(expiredYear == todayDate.year) {
-                if(expiredMonth < todayDate.month) {
-                    return true;
-                } else if(expiredMonth == todayDate.month) {
-                    if(expiredDay < todayDate.day) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        }
-    }
-    
 }
