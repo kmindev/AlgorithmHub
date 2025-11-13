8db0922a6d79dbbb17c6a3f999cb46874d41d196
Kang KyeongMin <kangkm0324@gmail.com>
2025-11-13 15:52:49 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1248 ms, Memory: 153964 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        for (int num : list) {
+            bw.write(num + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
